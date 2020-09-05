02647bf0bf867d8d80b9a9a8f74a02c2b2534cba
Geeronimo Alonso <geronimoalonso@icloud.com>
2020-09-05 17:09:52 -0300

    push 3

--- script.py
+++ script.py
@@ -10,19 +10,27 @@ import os
 
 def weekly_unsigned():
 	url = "https://buildium.na2.echosign.com/public/login"
+	print('url defined')
 	driver = webdriver.Chrome(ChromeDriverManager().install()) #set the search engine
 	driver.implicitly_wait(10) #set implicit wait
 	driver.get(url) #open the browser
+	print('driver selected')
 	adobe_password = os.environ.get(ADMIN_ADOBE_PASSWORD)
+	print('environment variable created')
 	login = driver.find_element_by_id("userEmail")
+	print('element found')
 	password = driver.find_element_by_id("userPassword")
+	print('password element found')
 	login.send_keys("admin@ibericmalls.com")
 	password.send_keys(adobe_password)
+	print('keys succesfully sent')
 	sign_in = driver.find_element_by_id("login")
 	sign_in.click()
+	print('you have already logged in')
 	manage = driver.find_element_by_xpath("//a[contains(text(), 'Manage')]")
 	manage.click()
 	pending_count = int(driver.find_element_by_id("1-OUT_FOR_SIGNATURE-count").text)
+	print('success')
 
 
 	docs_list = []
@@ -32,6 +40,7 @@ def weekly_unsigned():
 	subject = "PENDIENTES LEASES"	
 	body = "Documentos pendientes de firma: " + ", ".join(docs_list)
 	msg = f"Subject: {subject}\n\n{body}"
+	print('success 2')
 	print(msg)	
 	with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
 		smtp.ehlo()
@@ -41,11 +50,6 @@ def weekly_unsigned():
 		smtp.login("geronimo.joaquin.alonso@gmail.com", gmail_password)
 		smtp.sendmail("geronimo.joaquin.alonso@gmail.com", "commercial@ibericmalls.com", msg)
 		print("Reminder Sent")
-
-
-#for test process
-#python3 -m smtpd -c DebuggingServer -n localhost:1025
-#with smtplib.SMTP("localhost", 1025):
 
 
 schedule.every(10).seconds.do(weekly_unsigned)
