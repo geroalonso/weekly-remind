ddd4cd88b95d45c598836664bb8a1fcc5270d641
Geronimo Alonso <geronimoalonso@icloud.com>
2021-04-21 14:36:32 -0500

    anda todo joya, puse para que se envie el email diariamente a las 17

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('cron', hour=17)
 def timed_job():
 	url = "https://rest.tsheets.com/api/v1/timesheets"
 
